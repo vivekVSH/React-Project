3bdd7fed52940cc75ca7a8805595363ae496c334
vivekVSH <handevivek942@gmail.com>
2025-12-09 18:12:47 +0530

    form prevent default example

--- React-Hook-Advance/src/App.jsx
+++ React-Hook-Advance/src/App.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import { useState } from 'react'
+
 
 const App = () => {
+
+  const [obj , setObj] = useState({user : 'vivek' , age : 20});
+
+  function changeObj(){
+    let changeO = {...obj};
+    changeO.user = 'Yash';
+    changeO.age = 24;
+
+    setObj(changeO);
+  }
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    console.log("form is submited!!");
+  }
   return (
     <div>
-      Hello Man 
+      <h1>{obj.user}, {obj.age}</h1>
+      <button onClick={changeObj}>Click</button>
+
+      <form onSubmit={(e) => {
+        submitForm(e);
+      }}>
+        <input onChange={(e) => {
+          console.log(e.target.value);
+        }} type="text" placeholder='Enter Name'/>
+        <button>Submit</button>
+      </form>
     </div>
   )
 }
