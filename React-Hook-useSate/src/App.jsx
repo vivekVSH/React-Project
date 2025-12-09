19fb5502ec5c197da3ef332a418d35bb992802b5
vivekVSH <handevivek942@gmail.com>
2025-12-09 14:57:02 +0530

    add min project of increase and decrease

--- React-Hook-useSate/src/App.jsx
+++ React-Hook-useSate/src/App.jsx
@@ -1,34 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
 
+  const[num , steNum] = useState(0);
+
+  function increaseNum(){
+    steNum(num+1);
+  }
+
+  function decreaseNum(){
+    steNum(num-1);
+  }
+
+  function resetNum(){
+    steNum(0);
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>{num}</h1>
+      <button onClick={increaseNum}>Increase</button>
+      <button onClick={resetNum}>Reset</button>
+      <button onClick={decreaseNum}>Decrease</button>
+    </div>
   )
 }
 
